82c0b7c4e6707024f8daab8298496256ba9de30d
Pedro Martins <pedro.martins@GL05582M.local>
2016-07-13 22:53:30 +0100

    empty structure for start

--- src/gamesys/glsl-tester/main.js
+++ src/gamesys/glsl-tester/main.js
@@ -1,12 +1,8 @@
-var _program = require("./lesson-1/programes/p-triangle");
 var utils = require("./utils/utils");
 var glContext       = require('gl-context');
 var glClear    = require('gl-clear');
 module.exports = function () {
-  var gl;
-  var clear    = glClear({ color: [0.0, 0.0, 0.0, 1] })
   var viewSize = {width:700,height:500};
-  var program = _program(viewSize);
 
   /*https://developer.mozilla.org/en/docs/Web/API/HTMLCanvasElement/getContext*/
   var attributes = {
@@ -17,15 +13,6 @@ module.exports = function () {
   function initApp(gameProperties){
     gameProperties.canvas = utils.createElement(window,window.document.body,"canvas");
     utils.setSize(gameProperties.canvas,viewSize);
-    gl = glContext(gameProperties.canvas,attributes, render);
-    program.init(gl,gameProperties);
-  }
-  function render(){
-      clear(gl);
-      var width = gl.drawingBufferWidth;
-      var height = gl.drawingBufferHeight;
-      gl.viewport(0, 0, width, height);
-      program.render(width, height);
   }
   return {
       initialise: function () {
